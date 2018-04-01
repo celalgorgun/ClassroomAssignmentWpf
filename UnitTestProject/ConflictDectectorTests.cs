5e3aaf76f01da03ee813dc3b7af34fa03c77e009
Celal Gorgun <cgorgun@unomaha.edu>
2018-04-01 15:15:22 -0500

    Update ConflictDectectorTests.cs

--- UnitTestProject/ConflictDectectorTests.cs
+++ UnitTestProject/ConflictDectectorTests.cs
@@ -1,4 +1,4 @@
-﻿using ClassroomAssignment.Model.Repo;
+using ClassroomAssignment.Model.Repo;
 using ClassroomAssignment.Repo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -16,7 +16,7 @@ namespace UnitTestProject
         [TestMethod]
         public void HasConflict_True()
         {
-            ICourseRepository coureRepository = new ConflictingCourseRepo();
+            ICourseRepository courseRepository = new ConflictingCourseRepo();
             ConflictDetector detector = new ConflictDetector(courseRepository);
             ConflictResult result = detector.FindConflicts();
 
